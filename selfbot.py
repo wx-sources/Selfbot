22705f892d408a25d1670cdd04dd35f7beae22e6
Wx <alodotocanaru@gmail.com>
2025-08-01 16:29:51 -0300

    Create selfbot.py

---
+++ selfbot.py
@@ -0,0 +1,14 @@
+yndef on_ready():
+    (f"Logado como {bot.user}")
+
+@bot.command(name="print")
+async def print_command(ctx):
+    for _ in range(50):
+        await ctx.send("https://discord.gg/vFyYuxNe\n# NOSSA COM) print_command(ctx):
+    for _ in range(50):
+        await ctx.send("https://discord.gg/vFyYuxNe\n# NOSSA COMUNIDADE VENCEU")
+        await asyncio.sleep(1)  # Delay para evitar spam muito rápido
+
+bot.run(TOKEN, bot=False) rápido
+
+bot.run(TOKEN, bot=False)

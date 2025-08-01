59574818de531c45798f5efd6c357182ff9938bb
Wx <alodotocanaru@gmail.com>
2025-08-01 16:32:32 -0300

    Atualizar o selfbot.py

--- selfbot.py
+++ selfbot.py
@@ -1,14 +1,20 @@
-yndef on_ready():
-    (f"Logado como {bot.user}")
+import os
+import discord
+from discord.ext import commands
+import asyncio
+
+TOKEN = os.environ.get("Token_do_executor")
+
+bot = commands.Bot(command_prefix=".", self_bot=True)
+
+@bot.event
+async def on_ready():
+    print(f"Logado como {bot.user}")
 
 @bot.command(name="print")
 async def print_command(ctx):
     for _ in range(50):
-        await ctx.send("https://discord.gg/vFyYuxNe\n# NOSSA COM) print_command(ctx):
-    for _ in range(50):
         await ctx.send("https://discord.gg/vFyYuxNe\n# NOSSA COMUNIDADE VENCEU")
         await asyncio.sleep(1)  # Delay para evitar spam muito rápido
-
-bot.run(TOKEN, bot=False) rápido
 
 bot.run(TOKEN, bot=False)
